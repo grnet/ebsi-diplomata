f31d93dbbd6f6c6030aea93ed6d63ca64bf17e7e
fmerg <fmerg@grnet.gr>
2021-11-23 11:54:53 +0200

    Plug postgres db

--- issuer/entrypoint.sh
+++ issuer/entrypoint.sh
@@ -3,6 +3,11 @@
 HOST=0.0.0.0
 PORT=7000
 
+until pg_isready -h ${SQL_HOST} -p ${SQL_PORT}; do 
+    echo "Connecting to database..."
+    sleep 2
+done
+
 python3 manage.py makemigrations --noinput
 python3 manage.py makemigrations core --noinput
 python3 manage.py migrate --noinput
