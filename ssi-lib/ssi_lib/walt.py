eda03b1677c7a8859c20e8b9581e7d18b5d9e512
fmerg <fmerg@grnet.gr>
2021-12-10 11:42:05 +0200

    Create ssi_lib.walt module

---
+++ ssi-lib/ssi_lib/walt.py
@@ -0,0 +1,50 @@
+import json
+import os
+import subprocess
+
+def run_cmd(args):
+    rslt = subprocess.run(args, stdout=subprocess.PIPE)
+    resp = rslt.stdout.decode('utf-8').rstrip('\n')
+    code = rslt.returncode
+    return (resp, code)
+
+
+class WaltWrapper(object):
+
+    def __init__(self, tmpdir):
+        self.tmpdir = tmpdir
+
+    def _generate_key(self, algorithm, outfile):
+        res, code = run_cmd([
+            'generate-key', '--algo', algorithm, '--export', outfile,
+        ])
+        return res, code
+
+    def _load_key(self, alias):
+        outfile = os.path.join(self.tmpdir, 'jwk.json')
+        entry = self._db.get_entry(alias, _Group.KEY)
+        with open(outfile, 'w+') as f:
+            json.dump(entry, f)
+        res, code = run_cmd(['load-key', '--file', outfile])
+        os.remove(outfile)
+        return res, code
+
+    def _generate_did(self, key, outfile):
+        res, code = run_cmd([
+            'generate-did', '--key', key, '--export', outfile,
+        ])
+        return res, code
+
+    def _register_did(self, alias, token):
+        token_file = os.path.join(self.tmpdir, 'bearer-token.txt')
+        with open(token_file, 'w+') as f:
+            f.write(token)
+        res, code = run_cmd(['register-did', '--did', alias, 
+            '--token', token_file, '--resolve',
+        ])
+        os.remove(token_file)
+        return res, code
+
+    def _resolve_did(self, alias):
+        res, code = run_cmd(['resolve-did', '--did', alias,])
+        return res, code
