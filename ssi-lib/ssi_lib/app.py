8b801f958b1893373221203ad842982f7232004d
fmerg <fmerg@grnet.gr>
2021-12-15 23:26:18 +0200

    Remove ssi_lib/walt module

--- ssi-lib/ssi_lib/app.py
+++ ssi-lib/ssi_lib/app.py
@@ -1,7 +1,7 @@
-import json
 import os
+import json
+import subprocess
 from .db import DbConnector
-from .walt import WaltWrapper
 from .conf import _Group, _Vc
 
 
@@ -25,18 +25,86 @@ _commands = {
     # TODO: Add here more options
 }
 
-class SSIApp(WaltWrapper):
+def run_cmd(args):
+    rslt = subprocess.run(args, stdout=subprocess.PIPE)
+    resp = rslt.stdout.decode('utf-8').rstrip('\n')
+    code = rslt.returncode
+    return (resp, code)
+
+
+class SSIApp(object):
 
     def __init__(self, dbpath, tmpdir):
         self._db = DbConnector(dbpath)
-        self.tmpdir = tmpdir    # TODO: Maybe pass it as argument to methods
-        super().__init__(tmpdir)
+        self.tmpdir = tmpdir
 
     @classmethod
     def create(cls, config):
         dbpath = config['db']
         tmpdir = config['tmp']
         return cls(dbpath, tmpdir)
+
+    def _generate_key(self, algo, outfile):
+        res, code = run_cmd([
+            'generate-key', '--algo', algo, '--export', outfile,
+        ])
+        return res, code
+
+    def _load_key(self, alias):
+        outfile = os.path.join(self.tmpdir, 'jwk.json')
+        entry = self._db.get_entry(alias, _Group.KEY)
+        with open(outfile, 'w+') as f:
+            json.dump(entry, f)
+        res, code = run_cmd(['load-key', '--file', outfile])
+        os.remove(outfile)
+        return res, code
+
+    def _generate_did(self, key, outfile):
+        res, code = run_cmd([
+            'generate-did', '--key', key, '--export', outfile,
+        ])
+        return res, code
+
+    def _register_did(self, alias, token):
+        token_file = os.path.join(self.tmpdir, 'bearer-token.txt')
+        with open(token_file, 'w+') as f:
+            f.write(token)
+        res, code = run_cmd(['register-did', '--did', alias, 
+            '--token', token_file, '--resolve',
+        ])
+        os.remove(token_file)
+        return res, code
+
+    def _resolve_did(self, alias):
+        res, code = run_cmd(['resolve-did', '--did', alias,])
+        return res, code
+
+    def _issue_credential(self, holder, issuer, command, arguments,
+            outfile):
+        res, code = run_cmd([
+            command,
+            '--holder', holder,
+            '--issuer', issuer,
+            '--export', outfile,
+            *arguments,
+        ])
+        return res, code
+
+    def _generate_presentation(self, holder, credentials):
+        args = ['present-credentials', '--holder', holder]
+        for credential in credentials:
+            args += ['-c', credential,]
+        res, code = run_cmd(args)
+        return res, code
+
+    def _verify_presentation(self, presentation):
+        tmpfile = os.path.join(self.tmpdir, 'vp.json')
+        with open(tmpfile, 'w+') as f:
+            json.dump(presentation, f)
+        res, code = run_cmd([
+            'verify-credentials', '--presentation', tmpfile,])
+        os.remove(tmpfile)
+        return res, code
 
     def get_aliases(self, group):
         return self._db.get_aliases(group)
