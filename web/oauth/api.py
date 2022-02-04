e5bfee223875552f3ce9f2927d7297291c39a109
fmerg <fmerg@grnet.gr>
2022-02-04 12:54:51 +0200

    Install authlib

--- web/oauth/api.py
+++ web/oauth/api.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.views.decorators.http import require_http_methods
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from oauth.logic import authlib
 
 @require_http_methods(['GET',])
 def login(request):
